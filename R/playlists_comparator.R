b4e2e88b2e73dd7d9cff6a4e254e23cf3e45412d
mapama247 <mpamies247@gmail.com>
2019-10-04 12:18:36 +0200

    small changes main code

--- R/playlists_comparator.R
+++ R/playlists_comparator.R
@@ -1,9 +1,11 @@
 library(spotifyr)
+library(installr)
 
 Sys.setenv(SPOTIFY_CLIENT_ID = 'b5c4a61095b74ce597f886697704ea3c')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '6b2271624f0c49a9abb65255b84cb462')
 access_token <- get_spotify_access_token()
 
+load("sysdata.rda")
 #top50df <- read.csv("countries_uri.csv", header=TRUE, sep=",")
 
 compare_countries <- function(countries=list("DE","AR","AU","AT","BE","BO","BR","BG","CA","CZ","CL","CO","CR","DK","EC","SV","SK","ES","US","EE","PH","FI","FR","GR","GT","NL","HN","HK","HU","IN","ID","IE","IS","IL","IT","JP","LV","LT","LU","MY","MT","MX","NI","NO","NZ","PA","PY","PE","PL","PT","UK","DO","RO","SG","ZA","SE","SW","TH","TW","TK","UY","VN")){
@@ -20,7 +22,7 @@ compare_countries <- function(countries=list("DE","AR","AU","AT","BE","BO","BR",
 		uri_top_playlist <- as.vector( top50df[top50df$CODE==country,]$URI )
 		top_songs <- get_playlist( uri_top_playlist , fields=c("name","followers","tracks") , authorization=get_spotify_access_token() )
 		songs_id <- top_songs[["tracks"]][["items"]][["track.id"]]
-		followers <- c(followers,top_songs[["followers"]][["total"]])
+		followers <- c(followers,as.numeric(top_songs[["followers"]][["total"]]))
 		explicit <- c(explicit,sum(top_songs[["tracks"]][["items"]][["track.explicit"]])/length(songs_id))
 		playlists[[i]] <- songs_id
 		i <- i + 1
@@ -36,9 +38,13 @@ compare_countries <- function(countries=list("DE","AR","AU","AT","BE","BO","BR",
 		avg_features[nrow(avg_features)+1,1:11] <- apply(features,2,mean)
 	}
 	avg_features$code <- sapply(countries, paste0, collapse=",")
-	avg_features$followers <- sapply(followers, paste0, collapse=",")
+	avg_features$followers <- as.numeric(sapply(followers, paste0, collapse=","))
 	avg_features$expl <- sapply(explicit, paste0, collapse=",")
-	names(avg_features) <- c("anceability","energy","key","loudness","mode","speechiness","acousticness","instrumentalness","liveness","valence","tempo","code","followers","explicit")
+	names(avg_features) <- c("danceability","energy","key","loudness","mode","speechiness","acousticness","instrumentalness","liveness","valence","tempo","code","followers","explicit")
 	
 	return(avg_features)
 }
+
+l <- list("PT","ES")
+avgs <- compare_countries(l)
+#avgsALL <- compare_countries()
