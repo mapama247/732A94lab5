6fe5ad3b40797cc0ced13d081a3cdb87b5afa6ad
mapama247 <mpamies247@gmail.com>
2019-10-06 21:05:01 +0200

    import library in test file

--- tests/testthat/test_comparator.R
+++ tests/testthat/test_comparator.R
@@ -1,5 +1,10 @@
 context("playlists_comparator")
 
+library(spotifyr)
+Sys.setenv(SPOTIFY_CLIENT_ID = 'b5c4a61095b74ce597f886697704ea3c')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '6b2271624f0c49a9abb65255b84cb462')
+access_token <- get_spotify_access_token()
+
 load("sysdata.rda")
 
 test_that("Check if correct inputs", {
