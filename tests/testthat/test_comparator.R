496f98a06c659be5092aa651eddf54b5536c461d
mapama247 <mpamies247@gmail.com>
2019-10-06 20:44:43 +0200

    test cases updated

--- tests/testthat/test_comparator.R
+++ tests/testthat/test_comparator.R
@@ -1,13 +1,14 @@
-#context("playlists_comparator")
+context("playlists_comparator")
 
-#load("sysdata.rda")
+load("sysdata.rda")
 
-#test_that("Wrong inputs throws error", {
-#  expect_error(compare_countries(list("DE", "DE")))
-#  expect_error(compare_countries(list("CL", 1)))
-#  expect_error(compate_countries(list("ES", "AMXC")))
-#})
+test_that("Check if correct inputs", {
+  expect_error(compare_countries(list("CL", 1)))
+  expect_error(compate_countries(list("ES", "AMXC")))
+})
 
-#test_that("Check if numeric", {
-#	expect_equal(is.numeric(compare_countries(list("DE", "AR"))$energy), TRUE)
-#})
+test_that("Check if numeric outputs", {
+	expect_equal(is.numeric(compare_countries(list("DE", "AR"))$energy), TRUE)
+	expect_equal(is.numeric(get_followers()), TRUE)
+	expect_equal(is.numeric(get_explicit()), TRUE)
+})
